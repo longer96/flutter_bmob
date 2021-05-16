d8bfb82d01af85256145f78c95658b8d28f2c292
longer <wujunlong2011@qq.com>
2021-05-16 12:37:06 +0800

    加密初始化 短信发送的bug

--- lib/bmob/bmob_sms.dart
+++ lib/bmob/bmob_sms.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_bmob/bmob/bmob_dio.dart';
 import 'package:flutter_bmob/bmob/bmob.dart';
 
@@ -17,8 +19,14 @@ class BmobSms {
 
   ///查询单条数据
   Future<BmobSent> sendSms() async {
-    Map<String, dynamic> responseData = await BmobDio.getInstance()
-        .post(Bmob.BMOB_API_SEND_SMS_CODE, data: getParams());
+    // Map<String, dynamic> responseData = await BmobDio.getInstance()
+    //     .post(Bmob.BMOB_API_SEND_SMS_CODE, data: getParams());
+    // BmobSent sent = BmobSent.fromJson(responseData);
+    // return sent;
+
+    /// 上面的有bug  会导致加密验证失败
+    String params = json.encode(getParams());
+    Map<String, dynamic> responseData = await BmobDio.getInstance().post(Bmob.BMOB_API_SEND_SMS_CODE, data: params);
     BmobSent sent = BmobSent.fromJson(responseData);
     return sent;
   }
@@ -27,15 +35,14 @@ class BmobSms {
   Future<BmobHandled> verifySmsCode(smsCode) async {
     Map params = getParams();
     params.remove("template");
-    Map<String, dynamic> responseData = await BmobDio.getInstance()
-        .post(Bmob.BMOB_API_VERIFY_SMS_CODE + smsCode, data: params);
+    Map<String, dynamic> responseData =
+        await BmobDio.getInstance().post(Bmob.BMOB_API_VERIFY_SMS_CODE + smsCode, data: params);
     BmobHandled bmobHandled = BmobHandled.fromJson(responseData);
     return bmobHandled;
   }
 
   //此处与类名一致，由指令自动生成代码
-  factory BmobSms.fromJson(Map<String, dynamic> json) =>
-      _$BmobSmsFromJson(json);
+  factory BmobSms.fromJson(Map<String, dynamic> json) => _$BmobSmsFromJson(json);
 
   //此处与类名一致，由指令自动生成代码
   Map<String, dynamic> toJson() => _$BmobSmsToJson(this);
